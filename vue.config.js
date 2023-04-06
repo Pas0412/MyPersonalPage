1afe513f540ba11ad93c2cdfa54caa2a2aa93db0
黄永辉 <huangyh__123@163.com>
2023-04-06 22:44:33 +0200

    change public path for trying

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/MyPersonalPage/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/MyPersonalPage/" : "/MyPersonalPage/"
 })
